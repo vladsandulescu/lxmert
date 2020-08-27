db0bea643d72d0f0dd47ca9bb4b7f188c329a712
Vlad <vlad.sandulescu@gmail.com>
2020-08-27 12:46:53 +0200

    Update init_lxmert_ubuntu.sh

--- conda/init_lxmert_ubuntu.sh
+++ conda/init_lxmert_ubuntu.sh
@@ -6,6 +6,7 @@ conda activate lxmert
 
 cd $HOME/playground/hmm/lib/lxmert
 pip install -r requirements.txt
+pip install sklearn
 
 pip install ipykernel
 python -m ipykernel install --prefix=/home/ubuntu/anaconda3 --name lxmert --display-name "Python (lxmert)"
