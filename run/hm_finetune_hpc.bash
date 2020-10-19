0b8c2167d4df9cb89e8beb6fad412bd98e0f16bb
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2020-10-19 20:03:25 +0200

    adapt configs and dataset for phase 2

--- run/hm_finetune_hpc.bash
+++ run/hm_finetune_hpc.bash
@@ -2,7 +2,7 @@
 name=$2
 
 # Save logs and models under snap/hm; make backup.
-output=/playground/hmm/lib/lxmert/snap/hm/$name
+output=/work1/paupo/playground/hmm/lib/lxmert/snap/ph2_hm/$name
 mkdir -p $output/src
 cp -r src/* $output/src/
 cp $0 $output/run.bash
@@ -10,10 +10,10 @@ cp $0 $output/run.bash
 # See Readme.md for option details.
 CUDA_VISIBLE_DEVICES=$1 PYTHONPATH=$PYTHONPATH:./src \
     python src/tasks/hm.py \
-    --train train,dev --valid dev  \
+    --train train --valid dev  \
     --llayers 9 --xlayers 5 --rlayers 5 \
-    --loadLXMERT /playground/hmm/lib/lxmert/snap/pretrained/model \
+    --loadLXMERT /work1/paupo/playground/hmm/lib/lxmert/snap/pretrained/model \
     --batchSize 64 --optim bert --lr 2e-5 --epochs 5 \
-    --data_root /playground/hmm/data/ \
-    --imgfeat_root /playground/hmm/data/imgfeat/ \
+    --data_root /work1/paupo/playground/hmm/data_2/ \
+    --imgfeat_root /work1/paupo/playground/hmm/data_2/imgfeat/ \
     --tqdm --output $output ${@:3}
